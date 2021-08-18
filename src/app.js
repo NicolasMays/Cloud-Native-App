1e927ad33cf6da70ea03631351f7f07093d8d646
Nicolas Mays <nicolasmays@microsoft.com>
2021-08-18 15:54:55 -0400

    added dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+require('dotenv').config()
+
+let iKey = process.env.APPI_KEY
 let appInsights = require("applicationinsights");
-appInsights.setup("InstrumentationKey=78d645ae-119e-4999-aff7-71a0bbb6cc48;IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/").start();
-appInsights.start();
+appInsights.setup(iKey).start();
+
 const app = express();
 const port = 3000;
-
-
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
